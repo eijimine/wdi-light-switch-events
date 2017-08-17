0384a36dafec7e5bce1038da28fd8356aa297c66
eiji <eijimine@gmail.com>
2017-08-17 15:12:29 -0400

    Assignment Complete

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
 
+var clickswitch = document.querySelector('button.switch.on');
+var body = document.querySelector('body');
+var text = document.querySelector('h1.status');
 
+clickswitch.addEventListener('click', function(e) {
+  // e.preventDefault();
+  if (clickswitch.className === 'switch on') {
+      clickswitch.className = 'switch off';
+      body.className = 'dark';
+      text.innerText = 'Hey, who turned off the lights?';
+      text.innerText = 'Hey, who turned off the lights?';
+  } else if (clickswitch.className === 'switch off') {
+      clickswitch.className = 'switch on';
+      body.className = 'light';
+      text.innerText = 'It\'s so bright in here!';
+  }
+});
+});
